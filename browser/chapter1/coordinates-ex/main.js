713e78394579ed16745e16388706766faed7c565
RINLAND <summerland829@gmail.com>
2021-10-14 16:57:11 +0900

    211014: js dom

--- browser/chapter1/coordinates-ex/main.js
+++ browser/chapter1/coordinates-ex/main.js
@@ -2,6 +2,10 @@ const vertical = document.querySelector('.vertical');
 const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
+
 
 document.addEventListener('mousemove', function(event){
     // console.log('let');
@@ -9,12 +13,11 @@ document.addEventListener('mousemove', function(event){
     // console.log(`${event.clientY}`);
     const x = event.clientX;
     const y = event.clientY;
+    //translate  
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
 
-    vertical.style.left = `${x}px`;
-    horizontal.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-    tag.style.left = `${x}px`;
-    tag.style.top = `${y}px`;
+    target.style.transform = `translate(${x-targetHalfWidth}px, ${y-targetHalfHeight}px)`;
+    tag.style.transform = `translate(${x}px, ${y}px)`;
     tag.innerHTML= `${x}px, ${y}px`;
 });
