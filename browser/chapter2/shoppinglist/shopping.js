4cad6ecaae0caafe8e0e6346dcb614c26aa68304
RINLAND <summerland829@gmail.com>
2021-10-20 15:04:38 +0900

    211020 : Shopping list

--- browser/chapter2/shoppinglist/shopping.js
+++ browser/chapter2/shoppinglist/shopping.js
@@ -1,1 +1,18 @@
 'use strict';
+const plus = document.querySelector('.plus');
+const input = document.querySelector('input');
+// let value = document.getElementById('value').value;
+// let result = document.getElementById('#result').innerText;
+// input.addEventListener('input', function(e){
+//     log.textContent = e.target.value;
+//     console.log('world');
+// });
+
+
+plus.addEventListener('click', function(){
+    //input에 입력된 값을 stuff에 저장
+    const stuff = document.getElementById('stuff').value;
+    const newDiv = document.createElement('div');
+    newDiv.textContent = 'hello';
+ 
+});
