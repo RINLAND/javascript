9409ba5250fe9608f6a6474e9d1fab566670f618
RINLAND <summerland829@gmail.com>
2021-11-26 16:40:44 +0900

    211101 : Shopping list ex (js)

---
+++ browser/chapter2/shoppinglist-ex/main.js
@@ -0,0 +1,10 @@
+
+// DOM요소
+const items = document.querySelector('.items');
+const input = document.querySelector('.footer__input');
+const addBtn = document.querySelector('.footer__button');
+
+
+function onAdd() {
+    
+}
