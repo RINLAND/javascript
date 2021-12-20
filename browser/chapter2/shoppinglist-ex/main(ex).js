cc59ab0724d24e740e7fabd9e09305650bcb7697
RINLAND <summerland829@gmail.com>
2021-12-20 19:50:28 +0900

    211220 shoppinglist

--- browser/chapter2/shoppinglist-ex/main(ex).js
+++ browser/chapter2/shoppinglist-ex/main(ex).js
@@ -23,21 +23,35 @@ function onAdd() {
     input.value = '';
     input.focus();
 }
-let id = 0; //UUID 실제로 배포하는 레벨의 프로젝트가 아니기때문에 글로벌 숫자변수 이용
+
 function createItem(text){
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class', 'item__row' );
-    itemRow.setAttribute('data-id',id);
-    itemRow.innerHTML = `
-        <div class="item"  data-id=${id}>
-            <span class="item__name">${text}</span>
-            <button class="item__delete">
-                <i class="fas fa-trash-alt" data-id=${id}></i>
-            </button>
-        </div>
-        <div class="item__divider"></div>`
-    ;
-    id++;
+
+    const item = document.createElement('div');
+    item.setAttribute('class', 'item' );
+
+    const name = document.createElement('span');
+    name.setAttribute('class', 'item__name');
+    name.innerText = text;
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.setAttribute('class', 'item__delete');
+    deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    deleteBtn.addEventListener('click', function(){
+        items.removeChild(itemRow);
+    });
+
+    const itemDivider = document.createElement('div');
+    itemDivider.setAttribute('class', 'item__divider');
+
+
+    item.appendChild(name);
+    item.appendChild(deleteBtn);
+
+    itemRow.appendChild(item);
+    itemRow.appendChild(itemDivider);
+    
     return itemRow;
 }
 addBtn.addEventListener('click', function(){
@@ -48,13 +62,5 @@ input.addEventListener('keypress', function(event){
 
     if (event.key === 'Enter'){
         onAdd();
-    }
-})
-
-items.addEventListener('click', event =>{
-    const id = event.target.dataset.id;
-    if(id){
-        const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
-        toBeDeleted.remove();
     }
 })
