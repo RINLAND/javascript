ab76829115c122b46704467825319b402e79b70d
RINLAND <summerland829@gmail.com>
2021-12-23 08:28:58 +0900

    20211222 javascript info

--- javascriptinfo/main.js
+++ javascriptinfo/main.js
@@ -1,1 +1,9 @@
 alert ('Hello World!!');
+alert("hello");
+
+const hello = prompt('how old are you?', 100);
+alert(`당신의 나이는 ${hello} 입니다.`);
+
+const login = confirm("로그인 하시겠습니까?");
+alert(login);
+
